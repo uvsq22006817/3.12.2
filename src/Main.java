5379e3fcf548e0cd1ef9f4f42dec3b2d60bd03b2
uvsq22006817 <naimabouchrit15@gmail.com>
2021-10-09 01:16:47 +0200

    Implémentation du scénario

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        Client c1=new Client("Valérie");
+        Client c2=new Client("Mehdi");
+        Client c3=new Client("Mark");
+        Client c4=new Client("Steph");
+        Serveur s=new Serveur();
+        c1.seConnecter(s);
+        c2.seConnecter(s);
+        c3.seConnecter(s);
+        c4.seConnecter(s);
+        c1.envoyer("Bonjour");
+    }
 }
